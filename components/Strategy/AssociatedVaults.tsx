033e7db066fe1ca516d68e5196aed67d049ee9d5
Christopher Nascone <chris@nasc.one>
2022-08-23 10:10:58 -0400

    feat: show existing vaults on strategy page

---
+++ components/Strategy/AssociatedVaults.tsx
@@ -0,0 +1,72 @@
+import { useConfig } from 'hooks/useConfig';
+import Link from 'next/link';
+import React from 'react';
+import { VaultsByStrategyDocument } from 'types/generated/graphql/inKindSubgraph';
+import { useQuery } from 'urql';
+
+type Vault = {
+  __typename?: 'Vault' | undefined;
+  id: string;
+  tokenId: any;
+  debt: any;
+  open: boolean;
+  owner: {
+    __typename?: 'Account' | undefined;
+    id: string;
+  };
+};
+
+type AssociatedVaultsProps = {
+  strategy: string;
+};
+export function AssociatedVaults({ strategy }: AssociatedVaultsProps) {
+  const [{ data, fetching }] = useQuery({
+    query: VaultsByStrategyDocument,
+    variables: { strategy },
+  });
+  data?.vaults;
+  return (
+    <fieldset>
+      <legend>Associated Vaults</legend>
+      {fetching && <Loading />}
+      {!fetching && !!data && (
+        <Loaded strategy={strategy} vaults={data.vaults} />
+      )}
+    </fieldset>
+  );
+}
+
+function Loading() {
+  return <p>Loading vaults...</p>;
+}
+
+type LoadedProps = {
+  strategy: string;
+  vaults: Vault[];
+};
+function Loaded({ strategy, vaults }: LoadedProps) {
+  const { network } = useConfig();
+  if (vaults.length === 0) {
+    return <p>No vaults associated with this strategy</p>;
+  }
+
+  return (
+    <ul>
+      {vaults.map((v) => {
+        return (
+          <li key={v.id}>
+            <Link
+              href={`/network/${network}/strategies/${strategy}/${v.id.replace(
+                '0x',
+                '',
+              )}`}>
+              <a>
+                {v.id.substring(0, 16)}... ({v.open ? 'open' : 'closed'})
+              </a>
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
--- pages/network/[network]/in-kind/strategies/[strategy]/index.tsx
+++ pages/network/[network]/in-kind/strategies/[strategy]/index.tsx
@@ -3,7 +3,6 @@ import { LendingStrategy, populateLendingStrategy } from 'lib/strategies';
 import { GetServerSideProps } from 'next';
 import { useCallback, useEffect, useState } from 'react';
 import { ethers } from 'ethers';
-import { useNetwork } from 'wagmi';
 import MintERC20 from 'components/Strategy/MintERC20';
 import MintCollateral from 'components/Strategy/MintCollateral';
 import OpenVault from 'components/Strategy/OpenVault';
@@ -12,6 +11,8 @@ import SwapQuote from 'components/Strategy/SwapQuote';
 import StrategyState from 'components/Strategy/StrategyState';
 import ProvideLiquidity from 'components/Strategy/ProvideLiquidty';
 import SwapTokens from 'components/Strategy/SwapTokens';
+import styles from './strategy.module.css';
+import { AssociatedVaults } from 'components/Strategy/AssociatedVaults';
 
 export type StrategyPageProps = {
   address: string;
@@ -32,7 +33,6 @@ export const getServerSideProps: GetServerSideProps<StrategyPageProps> = async (
 const PRICE = 20_000;
 
 export default function StrategyPage({ address }: StrategyPageProps) {
-  const { chain } = useNetwork();
   const config = useConfig();
   const [lendingStrategy, setLendingStrategy] =
     useState<LendingStrategy | null>(null);
@@ -40,41 +40,46 @@ export default function StrategyPage({ address }: StrategyPageProps) {
   const populate = useCallback(async () => {
     const s = await populateLendingStrategy(address, config);
     setLendingStrategy(s);
-  }, [address]);
+  }, [address, config]);
 
   useEffect(() => {
     populate();
-  }, [address]);
+  }, [populate]);
 
   return (
     <div>
       <h3>Strategy</h3>
       <p>(fake) oracle price: {PRICE} </p>
       {lendingStrategy != null ? (
-        <div>
-          <StrategyState strategy={lendingStrategy} />
-          <p>
-            {lendingStrategy.name} ({lendingStrategy.symbol})
-          </p>
-          <PoolState pool={lendingStrategy.pool} />
-          <MintERC20 token={lendingStrategy.underlying} />
-          <MintCollateral token={lendingStrategy.collateral} />
-          <ProvideLiquidity pool={lendingStrategy.pool} />
-          <OpenVault strategy={lendingStrategy} />
-          <SwapQuote
-            tokenIn={lendingStrategy!.token0}
-            tokenOut={lendingStrategy!.token1}
-            fee={ethers.BigNumber.from(10).pow(4)} // 1% fee tier, should just fetch from pool directly
-          />
-          <SwapQuote
-            tokenIn={lendingStrategy!.token1}
-            tokenOut={lendingStrategy!.token0}
-            fee={ethers.BigNumber.from(10).pow(4)} // 1% fee tier, should just fetch from pool directly
-          />
-          <SwapTokens
-            tokenOne={lendingStrategy!.token0}
-            tokenTwo={lendingStrategy!.token1}
-          />
+        <div className={styles.wrapper}>
+          <div>
+            <StrategyState strategy={lendingStrategy} />
+            <p>
+              {lendingStrategy.name} ({lendingStrategy.symbol})
+            </p>
+            <PoolState pool={lendingStrategy.pool} />
+            <MintERC20 token={lendingStrategy.underlying} />
+            <MintCollateral token={lendingStrategy.collateral} />
+            <ProvideLiquidity pool={lendingStrategy.pool} />
+            <OpenVault strategy={lendingStrategy} />
+            <SwapQuote
+              tokenIn={lendingStrategy!.token0}
+              tokenOut={lendingStrategy!.token1}
+              fee={ethers.BigNumber.from(10).pow(4)} // 1% fee tier, should just fetch from pool directly
+            />
+            <SwapQuote
+              tokenIn={lendingStrategy!.token1}
+              tokenOut={lendingStrategy!.token0}
+              fee={ethers.BigNumber.from(10).pow(4)} // 1% fee tier, should just fetch from pool directly
+            />
+            <SwapTokens
+              tokenOne={lendingStrategy!.token0}
+              tokenTwo={lendingStrategy!.token1}
+            />
+          </div>
+          <div>
+            <AssociatedVaults strategy={address} />
+          </div>
         </div>
       ) : (
         ''
