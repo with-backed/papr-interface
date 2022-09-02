23b7b75ce47c3d017a85a9db8c28e704ffb13ab9
Christopher Nascone <chris@nasc.one>
2022-09-02 13:23:25 -0400

    feat: demo

--- components/Slider/Slider.tsx
+++ components/Slider/Slider.tsx
@@ -1,12 +1,49 @@
+import { Fieldset } from 'components/Fieldset';
+import { useState } from 'react';
 import ReactSlider from 'react-slider';
 import styles from './Slider.module.css';
 
+const squares = 'slider-squares';
+const dots = 'slider-dots';
+const dense = 'slider-dots-dense';
+
 export function Slider() {
+  const [style, setStyle] = useState(squares);
   return (
-    <ReactSlider
-      className={styles.slider}
-      trackClassName={styles.track}
-      thumbClassName={styles.thumb}
-    />
+    <>
+      <Fieldset legend="Slider Style" style={{ width: '498px' }}>
+        <label>
+          Squares{' '}
+          <input
+            type="radio"
+            checked={style === squares}
+            onChange={() => setStyle(squares)}
+          />
+        </label>
+        <br />
+        <label>
+          Dots{' '}
+          <input
+            type="radio"
+            checked={style === dots}
+            onChange={() => setStyle(dots)}
+          />
+        </label>
+        <br />
+        <label>
+          Dense Dots{' '}
+          <input
+            type="radio"
+            checked={style === dense}
+            onChange={() => setStyle(dense)}
+          />
+        </label>
+      </Fieldset>
+      <ReactSlider
+        className={styles[style]}
+        trackClassName={styles.track}
+        thumbClassName={styles.thumb}
+      />
+    </>
   );
 }
--- pages/network/[network]/borrow/index.tsx
+++ pages/network/[network]/borrow/index.tsx
@@ -4,6 +4,7 @@ import { captureException } from '@sentry/nextjs';
 import { SupportedNetwork, validateNetwork } from 'lib/config';
 import { OpenGraph } from 'components/OpenGraph';
 import capitalize from 'lodash/capitalize';
+import { Slider } from 'components/Slider';
 
 export const getServerSideProps: GetServerSideProps<BorrowProps> = async (
   context,
@@ -33,6 +34,7 @@ export default function Borrow({ network }: BorrowProps) {
     <>
       <OpenGraph title={`Backed | ${capitalize(network)} | Borrow`} />
       <h1>under construction</h1>
+      <Slider />
     </>
   );
 }
