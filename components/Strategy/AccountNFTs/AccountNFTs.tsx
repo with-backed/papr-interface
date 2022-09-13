2543c3d9e5daa81994e93fa1ae3fa7fc34b366a8
Adam Gobran <adamgobes@gmail.com>
2022-09-13 13:27:23 -0400

    NFT-510: Component to show user NFTs + approvals (#41)
    * progress
    
    * progress
    
    * hooks
    
    * approval loading
    
    * style changes re rinkeby
    
    * changes re cnasc

---
+++ components/Strategy/AccountNFTs/AccountNFTs.tsx
@@ -0,0 +1,214 @@
+import { Fieldset } from 'components/Fieldset';
+import { getAddress } from 'ethers/lib/utils';
+import { useCenterNFTs } from 'hooks/useCenterNFTs';
+import { useConfig } from 'hooks/useConfig';
+import { erc721Contract } from 'lib/contracts';
+import { LendingStrategy } from 'lib/strategies';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useAccount, useSigner } from 'wagmi';
+import styles from './AccountNFTs.module.css';
+
+export type AccountNFTsProps = {
+  strategy: LendingStrategy;
+};
+
+const getUniqueNFTId = (address: string, tokenId: string): string =>
+  `${getAddress(address)}-${tokenId}`;
+
+export default function AccountNFTs({ strategy }: AccountNFTsProps) {
+  const { address } = useAccount();
+  const config = useConfig();
+  const { data: signer } = useSigner();
+  const { userCollectionNFTs, nftsLoading } = useCenterNFTs(
+    address,
+    strategy.collateral.contract.address,
+    config,
+  );
+  const [nftsSelected, setNFTsSelected] = useState<string[]>([]);
+  const [nftsApproved, setNFTsApproved] = useState<string[]>([]);
+  const [approvalsLoading, setApprovalsLoading] = useState<{
+    [key: string]: boolean;
+  }>({});
+
+  const collateralContract = useMemo(() => {
+    return erc721Contract(strategy.collateral.contract.address, signer!);
+  }, [strategy, signer]);
+
+  const isNFTApproved = useCallback(
+    async (tokenId: string) => {
+      const approved =
+        getAddress(await collateralContract.getApproved(tokenId)) ===
+          getAddress(strategy.contract.address) ||
+        (await collateralContract.isApprovedForAll(
+          address!,
+          strategy.contract.address,
+        ));
+      return approved;
+    },
+    [strategy, collateralContract, address],
+  );
+
+  const initializeNFTsApproved = useCallback(async () => {
+    const nftApprovals = await Promise.all(
+      userCollectionNFTs.map(async (nft) => {
+        return (await isNFTApproved(nft.tokenId))
+          ? getUniqueNFTId(nft.address, nft.tokenId)
+          : '';
+      }),
+    );
+    setNFTsApproved(nftApprovals.filter((id) => !!id));
+    setNFTsSelected(nftsApproved);
+  }, [userCollectionNFTs, isNFTApproved]);
+
+  useEffect(() => {
+    if (nftsLoading) return;
+    initializeNFTsApproved();
+  }, [, nftsLoading]);
+
+  const handleNFTSelected = useCallback(
+    (address: string, tokenId: string, checked: boolean) => {
+      setNFTsSelected((prevNFTSelected) => {
+        const uniqueNFTId = getUniqueNFTId(address, tokenId);
+        return checked
+          ? [...prevNFTSelected, uniqueNFTId]
+          : prevNFTSelected.filter((s) => s !== uniqueNFTId);
+      });
+    },
+    [setNFTsSelected],
+  );
+
+  const noneSelected = useMemo(() => {
+    return nftsSelected.length === 0;
+  }, [nftsSelected]);
+
+  const performSelectAll = useCallback(() => {
+    setNFTsSelected(
+      userCollectionNFTs.map((nft) => getUniqueNFTId(nft.address, nft.tokenId)),
+    );
+  }, [userCollectionNFTs, setNFTsSelected]);
+
+  const performUnselectAll = useCallback(() => {
+    setNFTsSelected([]);
+  }, [setNFTsSelected]);
+
+  const approveNFT = useCallback(
+    async (tokenId: string) => {
+      setApprovalsLoading((prevApprovalsLoading) => ({
+        ...prevApprovalsLoading,
+        [getUniqueNFTId(collateralContract.address, tokenId)]: true,
+      }));
+      const t = await collateralContract.approve(
+        strategy.contract.address,
+        tokenId,
+      );
+      t.wait().then(() => {
+        setApprovalsLoading((prevApprovalsLoading) => ({
+          ...prevApprovalsLoading,
+          [getUniqueNFTId(collateralContract.address, tokenId)]: false,
+        }));
+        setNFTsApproved((prevNFTsApproved) => [
+          ...prevNFTsApproved,
+          getUniqueNFTId(collateralContract.address, tokenId),
+        ]);
+      });
+    },
+    [strategy, collateralContract, setApprovalsLoading],
+  );
+
+  const performApproveAll = useCallback(async () => {
+    const t = await collateralContract.setApprovalForAll(
+      strategy.contract.address,
+      true,
+    );
+  }, [collateralContract, strategy]);
+
+  if (nftsLoading) return <></>;
+
+  return (
+    <Fieldset legend="🖼️ Vault">
+      <div className={styles.wrapper}>
+        <div className={styles.nfts}>
+          <ol>
+            <li className={`${styles.row} ${styles.columnLabels}`}>
+              <div className={styles.imageTokenId}></div>
+              <div className={styles.oracleCheckBox}>
+                <div>
+                  <p>oracle</p>
+                </div>
+                <div>
+                  <p>deposit</p>
+                </div>
+              </div>
+            </li>
+            {userCollectionNFTs.map((nft, i) => (
+              <li
+                className={`${styles.row} ${i % 2 === 0 ? styles.even : ''}`}
+                key={`${nft.address}-${nft.tokenId}`}>
+                <div className={styles.imageTokenId}>
+                  <div>
+                    <img src={nft.smallPreviewImageUrl} />
+                  </div>
+                  <div>#{nft.tokenId}</div>
+                </div>
+                <div className={styles.oracleCheckBox}>
+                  <div>
+                    <p>$72,188</p>
+                  </div>
+                  <div>
+                    {nftsApproved.includes(
+                      getUniqueNFTId(nft.address, nft.tokenId),
+                    ) ? (
+                      <input
+                        type="checkbox"
+                        checked={nftsSelected.includes(
+                          getUniqueNFTId(nft.address, nft.tokenId),
+                        )}
+                        onChange={(e) =>
+                          handleNFTSelected(
+                            nft.address,
+                            nft.tokenId,
+                            e.target.checked,
+                          )
+                        }
+                      />
+                    ) : (
+                      <>
+                        {!approvalsLoading[
+                          getUniqueNFTId(nft.address, nft.tokenId)
+                        ] && (
+                          <p
+                            className={styles.approve}
+                            onClick={() => approveNFT(nft.tokenId)}>
+                            Approve
+                          </p>
+                        )}
+                        {approvalsLoading[
+                          getUniqueNFTId(nft.address, nft.tokenId)
+                        ] && <p>...</p>}
+                      </>
+                    )}
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ol>
+        </div>
+        <div className={styles.selectAll}>
+          {nftsApproved.length > 0 && (
+            <>
+              {noneSelected && <p onClick={performSelectAll}>Select All</p>}
+              {!noneSelected && (
+                <p onClick={performUnselectAll}>Unselect All</p>
+              )}
+            </>
+          )}
+          {nftsApproved.length === 0 && (
+            <>
+              <p onClick={performApproveAll}>Approve All</p>
+            </>
+          )}
+        </div>
+      </div>
+    </Fieldset>
+  );
+}
---
+++ hooks/useCenterNFTs.ts
@@ -0,0 +1,50 @@
+import { getAddress } from 'ethers/lib/utils';
+import { Config } from 'lib/config';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+
+type RenderUserNFTsResponse = {
+  address: string;
+  smallPreviewImageUrl: string;
+  tokenId: string;
+};
+
+export const useCenterNFTs = (
+  address: string | undefined,
+  collection: string,
+  config: Config,
+) => {
+  const [allUserNFTs, setAllUserNFTs] = useState<RenderUserNFTsResponse[]>([]);
+  const [nftsLoading, setNFTsLoading] = useState<boolean>(true);
+
+  const getAllUserNFTs = useCallback(async () => {
+    try {
+      const response = await fetch(
+        `https://api.center.dev/v1/${config.centerNetwork}/account/${address}/assets-owned`,
+        {
+          headers: {
+            'X-API-Key': process.env.NEXT_PUBLIC_CENTER_KEY!,
+          },
+        },
+      );
+      const json = await response.json();
+      setAllUserNFTs(json.items);
+      setNFTsLoading(false);
+    } catch (e) {
+      console.error(e);
+      setNFTsLoading(false);
+    }
+  }, [address, config]);
+
+  const userCollectionNFTs = useMemo(() => {
+    return allUserNFTs.filter(
+      (nft) => getAddress(nft.address) === getAddress(collection),
+    );
+  }, [allUserNFTs]);
+
+  useEffect(() => {
+    if (!address) return;
+    getAllUserNFTs();
+  }, [address]);
+
+  return { userCollectionNFTs, nftsLoading };
+};
