95f8c76072df911672b63bd929a97b22c7a5b042
Christopher Nascone <chris@nasc.one>
2022-09-21 12:08:44 -0400

    fix: still need to mint erc20 and collateral

---
+++ components/Strategy/MintCollateral.tsx
@@ -0,0 +1,49 @@
+import { Fieldset } from 'components/Fieldset';
+import { ERC721Token } from 'lib/strategies';
+import { useCallback, useEffect, useState } from 'react';
+import { MockCollateral__factory } from 'types/generated/abis';
+import { useAccount, useSigner } from 'wagmi';
+
+type MintCollateralProps = {
+  token: ERC721Token;
+};
+
+export default function MintCollateral({ token }: MintCollateralProps) {
+  const [balance, setBalance] = useState<string>('');
+  const { address } = useAccount();
+  const { data: signer } = useSigner();
+
+  const getBalance = useCallback(async () => {
+    if (address) {
+      const b = await token.contract.balanceOf(address);
+      setBalance(b.toString());
+    }
+  }, [address, token]);
+
+  const mint = useCallback(async () => {
+    if (signer == null || address == null) {
+      console.log('address or sigenr null');
+      return;
+    }
+    const contract = MockCollateral__factory.connect(
+      token.contract.address,
+      signer,
+    );
+    const t = await contract.mint(address);
+    await t.wait();
+    getBalance();
+  }, [address, getBalance, token, signer]);
+
+  useEffect(() => {
+    getBalance();
+  }, [getBalance]);
+
+  return (
+    <Fieldset legend={`➕ Mint yourself ${token.symbol}`}>
+      <p> your balance: {balance || 'not connected'} </p>
+      <button disabled={!address} onClick={mint}>
+        mint
+      </button>
+    </Fieldset>
+  );
+}
---
+++ components/Strategy/MintERC20.tsx
@@ -0,0 +1,58 @@
+import { Fieldset } from 'components/Fieldset';
+import { ethers } from 'ethers';
+import { ERC20Token } from 'lib/strategies';
+import { useCallback, useEffect, useState } from 'react';
+import { MockUnderlying__factory } from 'types/generated/abis';
+import { useAccount, useSigner } from 'wagmi';
+
+type TokenInfoProps = {
+  token: ERC20Token;
+};
+
+export default function MintERC20({ token }: TokenInfoProps) {
+  const [balance, setBalance] = useState<string>('');
+  const [value, setValue] = useState<string>('');
+  const { address } = useAccount();
+  const { data: signer } = useSigner();
+
+  const getBalance = useCallback(async () => {
+    if (address) {
+      const b = await token.contract.balanceOf(address);
+      setBalance(ethers.utils.formatUnits(b, token.decimals));
+    }
+  }, [address, token]);
+
+  const mint = useCallback(async () => {
+    if (signer == null || address == null) {
+      console.log('address or sigenr null');
+      return;
+    }
+    const contract = MockUnderlying__factory.connect(
+      token.contract.address,
+      signer,
+    );
+    ethers.utils.parseUnits(value, token.decimals);
+    const t = await contract.mint(
+      address,
+      ethers.utils.parseUnits(value, token.decimals),
+    );
+    await t.wait();
+    getBalance();
+  }, [address, getBalance, token, signer, value]);
+
+  useEffect(() => {
+    getBalance();
+  }, [getBalance]);
+
+  return (
+    <Fieldset legend={`🪙 Mint yourself ${token.symbol}`}>
+      <p> your balance: {balance || 'not connected'} </p>
+      <input
+        placeholder={'amount'}
+        onChange={(e) => setValue(e.target.value)}></input>
+      <button disabled={!address} onClick={mint}>
+        mint
+      </button>
+    </Fieldset>
+  );
+}
