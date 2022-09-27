3bc38fb5875979ae8dbb8d455846e4c02a93c2dd
Christopher Nascone <chris@nasc.one>
2022-09-27 15:02:55 -0400

    feat: borrow header link works

---
+++ pages/network/[network]/borrow/[strategyAddress]/index.tsx
@@ -0,0 +1,79 @@
+import { SupportedNetwork } from 'lib/config';
+import { strategyPricesData, StrategyPricesData } from 'lib/strategies/charts';
+import { GetServerSideProps } from 'next';
+import {
+  BorrowPageContent,
+  BorrowPageProps,
+} from 'components/Strategy/BorrowPageContent';
+import {
+  fetchSubgraphData,
+  makeLendingStrategy,
+  SubgraphPool,
+  SubgraphStrategy,
+} from 'lib/LendingStrategy';
+import { useConfig } from 'hooks/useConfig';
+import { useSigner } from 'wagmi';
+import { useEffect, useMemo, useState } from 'react';
+
+type ServerSideProps = Omit<
+  BorrowPageProps,
+  'lendingStrategy' | 'pricesData'
+> & {
+  subgraphStrategy: SubgraphStrategy;
+  subgraphPool: SubgraphPool;
+};
+
+export const getServerSideProps: GetServerSideProps<ServerSideProps> = async (
+  context,
+) => {
+  const address = (context.params?.strategyAddress as string).toLowerCase();
+  const network = context.params?.network as SupportedNetwork;
+
+  const strategySubgraphData = await fetchSubgraphData(address);
+
+  if (!strategySubgraphData) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const { pool, lendingStrategy } = strategySubgraphData;
+
+  return {
+    props: {
+      strategyAddress: address,
+      subgraphStrategy: lendingStrategy,
+      subgraphPool: pool,
+    },
+  };
+};
+
+export default function Borrow({
+  strategyAddress,
+  subgraphStrategy,
+  subgraphPool,
+}: ServerSideProps) {
+  const config = useConfig();
+  const { data: signer } = useSigner();
+
+  const lendingStrategy = useMemo(() => {
+    return makeLendingStrategy(subgraphStrategy, subgraphPool, signer!, config);
+  }, [config, signer, subgraphPool, subgraphStrategy]);
+
+  const [pricesData, setPricesData] = useState<StrategyPricesData | null>(null);
+
+  useEffect(() => {
+    strategyPricesData(
+      lendingStrategy,
+      config.network as SupportedNetwork,
+    ).then(setPricesData);
+  }, [lendingStrategy, config]);
+
+  return (
+    <BorrowPageContent
+      lendingStrategy={lendingStrategy}
+      strategyAddress={strategyAddress}
+      pricesData={pricesData}
+    />
+  );
+}
