98d1d85e0bfc12dc0da783e76575eb7fc98757ce
Christopher Nascone <chris@nasc.one>
2022-10-05 16:42:39 -0400

    feat: do price chart

--- components/Strategies/Charts/Charts.tsx
+++ components/Strategies/Charts/Charts.tsx
@@ -1,12 +1,9 @@
 import { Fieldset } from 'components/Fieldset';
 import { StrategyPricesData } from 'lib/strategies/charts';
-import React, { useEffect, useMemo, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from './Charts.module.css';
-import dynamic from 'next/dynamic';
 import { createChart, LineStyle, UTCTimestamp } from 'lightweight-charts';
-
-// apexcharts uses `window`, so will break if we SSR
-const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
+import { formatPercent, formatTokenAmount } from 'lib/numberFormat';
 
 type ChartsProps = {
   pricesData: StrategyPricesData | null;
@@ -29,45 +26,6 @@ export function Charts({ pricesData }: ChartsProps) {
   );
 }
 
-const baseOptions = {
-  chart: {
-    animations: {
-      enabled: false,
-    },
-    background: '#ffffff',
-    toolbar: {
-      show: false,
-    },
-  },
-  xaxis: {
-    type: 'datetime',
-    labels: {
-      formatter: function (_value: any, timestamp: any, opts: any) {
-        return opts.dateFormatter(new Date(timestamp * 1000), 'dd MMM');
-      },
-      style: {
-        cssClass: styles['chart-label'],
-      },
-    },
-  },
-  yaxis: {
-    labels: {
-      show: false,
-    },
-  },
-  colors: ['#007155', '#000000', '#000000'],
-  stroke: {
-    width: [3, 3, 3],
-    curve: 'smooth',
-    dashArray: [0, 4, 0],
-  },
-};
-
-const percentFormatter = new Intl.NumberFormat('en-US', {
-  style: 'percent',
-  minimumFractionDigits: 2,
-});
-
 type RateOfGrowthProps = {
   pricesData: StrategyPricesData;
 };
@@ -82,6 +40,10 @@ function RateOfGrowth({
         height: 360,
         grid: { horzLines: { visible: false } },
         layout: { textColor: '#ffffff' },
+        localization: {
+          priceFormatter: (value: string) =>
+            formatPercent(parseFloat(value) / 100),
+        },
       });
       chart
         .priceScale()
@@ -145,47 +107,73 @@ function PriceInUSDC({
     markValues,
   },
 }: PriceInUSDCProps) {
-  const options = {
-    ...baseOptions,
-  };
+  const chartRef = useRef<HTMLDivElement>(null);
 
-  const series = useMemo(
-    () => [
-      {
-        name: 'Contract',
-        data: normalizationDPRValues.map(([_, timestamp], i) => ({
-          x: timestamp,
-          y: parseFloat(normalizationValues[i]),
+  useEffect(() => {
+    if (chartRef.current) {
+      const chart = createChart(chartRef.current, {
+        height: 360,
+        grid: { horzLines: { visible: false } },
+        layout: { textColor: '#ffffff' },
+        localization: {
+          priceFormatter: (value: string) =>
+            formatTokenAmount(parseFloat(value)) + ' USDC',
+        },
+      });
+      chart
+        .priceScale()
+        .applyOptions({ drawTicks: false, borderVisible: false });
+      chart.timeScale().applyOptions({
+        visible: false,
+      });
+      const indexSeries = chart.addLineSeries({
+        lineStyle: LineStyle.SparseDotted,
+        color: '#000000',
+        priceLineVisible: false,
+      });
+      indexSeries.setData(
+        indexDPRValues.map(([_, timestamp]) => ({
+          time: timestamp as UTCTimestamp,
+          value: index,
         })),
-      },
-      {
-        name: 'Target',
-        data: indexDPRValues.map(([_, timestamp]) => ({
-          x: timestamp,
-          y: index,
-        })),
-      },
-      {
-        name: 'Market',
-        data: markDPRValues.map(([_, timestamp], i) => ({
-          x: timestamp,
-          y: markValues[i],
-        })),
-      },
-    ],
-    [
-      index,
-      indexDPRValues,
-      markDPRValues,
-      markValues,
-      normalizationDPRValues,
-      normalizationValues,
-    ],
-  );
+      );
 
-  return (
-    <Chart options={options as any} series={series} type="line" width="580" />
-  );
+      const markSeries = chart.addLineSeries({
+        color: '#007155',
+        priceLineVisible: false,
+      });
+      markSeries.setData(
+        markDPRValues.map(([_, timestamp], i) => ({
+          time: timestamp as UTCTimestamp,
+          value: markValues[i],
+        })),
+      );
+
+      const normSeries = chart.addLineSeries({
+        color: '#000000',
+        priceLineVisible: false,
+      });
+      normSeries.setData(
+        normalizationDPRValues.map(([_, timestamp], i) => ({
+          time: timestamp as UTCTimestamp,
+          value: parseFloat(normalizationValues[i]),
+        })),
+      );
+
+      chart.timeScale().fitContent();
+
+      return () => chart.remove();
+    }
+  }, [
+    index,
+    indexDPRValues,
+    markDPRValues,
+    markValues,
+    normalizationDPRValues,
+    normalizationValues,
+  ]);
+
+  return <div ref={chartRef} />;
 }
 
 export default Charts;
