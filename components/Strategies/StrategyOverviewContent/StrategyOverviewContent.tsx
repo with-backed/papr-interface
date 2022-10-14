2214efeaf2346d2ff987b58df7b42f62c15dccf6
Christopher Nascone <chris@nasc.one>
2022-10-14 10:51:57 -0400

    fix: emoji + import cleanup

--- components/Strategies/StrategyOverviewContent/StrategyOverviewContent.tsx
+++ components/Strategies/StrategyOverviewContent/StrategyOverviewContent.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import styles from 'components/Strategies/Strategy.module.css';
 import { StrategyPricesData } from 'lib/strategies/charts';
 import { LendingStrategy } from 'lib/LendingStrategy';
-import { Collateral } from '../Collateral';
-import { Activity } from '../Activity';
-import { Loans } from '../Loans';
+import { Collateral } from 'components/Strategies/Collateral';
+import { Activity } from 'components/Strategies/Activity';
+import { Loans } from 'components/Strategies/Loans';
 import StrategySummary from 'components/StrategySummary/StrategySummary';
 import dynamic from 'next/dynamic';
+import { YourPositions } from 'components/Strategies/YourPositions';
 
 /* lightweight-charts uses canvas and cannot be SSRed */
 const Charts = dynamic(() => import('components/Strategies/Charts/Charts'), {
@@ -26,9 +27,10 @@ export function StrategyOverviewContent({
 }: StrategyPageProps) {
   return (
     <div className={styles.wrapper}>
+      <YourPositions lendingStrategy={lendingStrategy} />
       <StrategySummary
         includeDetails={false}
-        legend="Token Performance"
+        legend="📈 Token Performance"
         pricesData={{ [lendingStrategy.id]: pricesData }}
         strategies={[lendingStrategy]}
       />
