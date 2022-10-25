85fd8ee11b10c38293746e543d222df7f8dfcd06
Christopher Nascone <chris@nasc.one>
2022-10-25 09:57:58 -0400

    feat: collateral view on vault page

--- components/Strategies/VaultPageContent/VaultPageContent.tsx
+++ components/Strategies/VaultPageContent/VaultPageContent.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { LendingStrategy } from 'lib/LendingStrategy';
 import { LoanDetails } from 'components/Strategies/Loans/LoanDetails';
 import strategyStyles from '../Strategy.module.css';
+import { Collateral } from 'components/Strategies/Collateral';
 
 type VaultPageContentProps = {
   lendingStrategy: LendingStrategy;
@@ -15,6 +16,7 @@ export function VaultPageContent({
   return (
     <div className={strategyStyles.wrapper}>
       <LoanDetails vaultId={vaultId} lendingStrategy={lendingStrategy} />
+      <Collateral lendingStrategy={lendingStrategy} vaultId={vaultId} />
     </div>
   );
 }
