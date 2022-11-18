c2ea4c5d40c022acaf498c7b2cd28150a611b02c
Christopher Nascone <chris@nasc.one>
2022-11-18 13:32:37 -0500

    fix: approximatelyEquals

--- __tests__/lib/auctions.test.ts
+++ __tests__/lib/auctions.test.ts
@@ -1,10 +1,16 @@
 import { ethers } from 'ethers';
 import { currentPrice } from 'lib/auctions';
 import { ONE } from 'lib/constants';
+import { convertOneScaledValue } from 'lib/controllers';
 
 const oneDayInSeconds = 60 * 60 * 24;
 const startPrice = ethers.BigNumber.from(10).pow(18);
-const perPeriodDecayPercentWad = ONE.mul(9).div(10).div(ONE).toNumber();
+const perPeriodDecayPercentWad = convertOneScaledValue(ONE.mul(9).div(10), 8);
+
+function approximatelyEquals(n1: ethers.BigNumber, n2: ethers.BigNumber) {
+  const tolerance = ethers.utils.parseEther('0.01');
+  return n1.sub(n2).abs().lt(tolerance);
+}
 
 describe('auction library', () => {
   describe('currentPrice', () => {
@@ -24,11 +30,9 @@ describe('auction library', () => {
         oneDayInSeconds,
         perPeriodDecayPercentWad,
       );
-      console.log({
-        result: ethers.utils.formatEther(result),
-        expected: ethers.utils.formatEther(ethers.BigNumber.from(10).pow(17)),
-      });
-      expect(result).toEqual(ethers.BigNumber.from(10).pow(17));
+      expect(
+        approximatelyEquals(result, ethers.BigNumber.from(10).pow(17)),
+      ).toBeTruthy();
     });
   });
 });
