220ef13b8c727948da62d3ea82b8a17e1d7d4d94
Adam Gobran <adamgobes@gmail.com>
2022-12-01 11:39:21 -0500

    fix: Render LTV correctly on collateral tile (#169)
    * fix: Render LTV correctly on collateral tile
    
    * refactor
    
    * fix

--- hooks/useLTVs/useLTVs.ts
+++ hooks/useLTVs/useLTVs.ts
@@ -1,6 +1,6 @@
 import { ethers } from 'ethers';
 import { useOracleInfo } from 'hooks/useOracleInfo/useOracleInfo';
-import { formatBigNum } from 'lib/numberFormat';
+import { computeLTVFromDebts } from 'lib/controllers';
 import { OraclePriceType } from 'lib/oracle/reservoir';
 import { PaprController } from 'lib/PaprController';
 import { useEffect, useState } from 'react';
@@ -21,18 +21,15 @@ export function useLTVs(
         const maxDebtForVault: ethers.BigNumber = (
           await paprController.maxDebt([v.collateralContract], oracleInfo)
         ).mul(v.collateral.length);
-        const maxNumber = parseFloat(
-          formatBigNum(maxDebtForVault, paprController.debtToken.decimals),
-        );
-        const debtNumber = parseFloat(
-          formatBigNum(v.debt, paprController.debtToken.decimals),
-        );
 
         return {
           ...prev,
-          [v.id]:
-            (debtNumber / maxNumber) *
-            parseFloat(ethers.utils.formatEther(maxLTV)),
+          [v.id]: computeLTVFromDebts(
+            ethers.BigNumber.from(v.debt),
+            maxDebtForVault,
+            maxLTV,
+            paprController.debtToken.decimals,
+          ),
         };
       }, {});
     };
