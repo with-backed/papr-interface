42caa3f0e8fea4a96cd783e344aff310019c87ec
Wilson Cusack <cusackw@gmail.com>
2022-12-01 12:26:53 -0500

    add try catch back

--- pages/tokens/[token]/competition.tsx
+++ pages/tokens/[token]/competition.tsx
@@ -18,69 +18,69 @@ import { ethers } from 'ethers';
 export const getServerSideProps: GetServerSideProps<
   HeroesLandingPageProps
 > = async (context) => {
-  // try {
-  validateToken(context.params!);
-  const token = context.params?.token as SupportedToken;
+  try {
+    validateToken(context.params!);
+    const token = context.params?.token as SupportedToken;
 
-  const controllerSubgraphData = await fetchSubgraphData(
-    configs[token].controllerAddress,
-    configs[token].uniswapSubgraph,
-    token,
-  );
+    const controllerSubgraphData = await fetchSubgraphData(
+      configs[token].controllerAddress,
+      configs[token].uniswapSubgraph,
+      token,
+    );
 
-  if (!controllerSubgraphData) {
+    if (!controllerSubgraphData) {
+      return {
+        notFound: true,
+      };
+    }
+
+    const { paprController } = controllerSubgraphData;
+    const allowedCollateral = paprController.allowedCollateral.map(
+      (ac: any) => ac.contractAddress,
+    );
+    const underlying = paprController.underlying;
+    const paprToken = paprController.paprToken;
+
+    const oracleInfo = await getOracleInfoFromAllowedCollateral(
+      allowedCollateral,
+      token,
+    );
+
+    const participatingPlayers = Array.from(
+      new Set(await getAllPaprHeroPlayers(token)),
+    );
+
+    const paprPrice = await getQuoteForSwap(
+      ethers.BigNumber.from(1),
+      paprToken,
+      underlying,
+      'paprHero',
+    );
+    const playerScores: [string, HeroPlayerBalance][] = await Promise.all(
+      participatingPlayers.map(async (p) => [
+        p.id,
+        await calculateNetPhUSDCBalance(p, paprPrice, oracleInfo, underlying),
+      ]),
+    );
+
+    const rankedPlayers = playerScores.sort(
+      (a, b) => b[1].totalBalance - a[1].totalBalance,
+    );
+
+    return {
+      props: {
+        token,
+        allowedCollateral,
+        oracleInfo,
+        rankedPlayers,
+      },
+    };
+  } catch (e) {
+    captureException(e);
     return {
       notFound: true,
     };
   }
-
-  const { paprController } = controllerSubgraphData;
-  const allowedCollateral = paprController.allowedCollateral.map(
-    (ac: any) => ac.contractAddress,
-  );
-  const underlying = paprController.underlying;
-  const paprToken = paprController.paprToken;
-
-  const oracleInfo = await getOracleInfoFromAllowedCollateral(
-    allowedCollateral,
-    token,
-  );
-
-  const participatingPlayers = Array.from(
-    new Set(await getAllPaprHeroPlayers(token)),
-  );
-
-  const paprPrice = await getQuoteForSwap(
-    ethers.BigNumber.from(1),
-    paprToken,
-    underlying,
-    'paprHero',
-  );
-  const playerScores: [string, HeroPlayerBalance][] = await Promise.all(
-    participatingPlayers.map(async (p) => [
-      p.id,
-      await calculateNetPhUSDCBalance(p, paprPrice, oracleInfo, underlying),
-    ]),
-  );
-
-  const rankedPlayers = playerScores.sort(
-    (a, b) => b[1].totalBalance - a[1].totalBalance,
-  );
-
-  return {
-    props: {
-      token,
-      allowedCollateral,
-      oracleInfo,
-      rankedPlayers,
-    },
-  };
-  // } catch (e) {
-  //   captureException(e);
-  //   return {
-  //     notFound: true,
-  //   };
-  // }
 };
 
 type HeroesLandingPageProps = {
