f463153fcb764671bf7a610db18a457743c01192
Christopher Nascone <cnasc@users.noreply.github.com>
2022-12-02 12:04:02 -0500

    [NFT-645] fix:  vault health (#182)
    * feat: standardize on ltv-based vault health
    
    * feat: basic tooltip
    
    * fix: pointer
    
    * hotfix: chart margin

--- components/Controllers/Loans/VaultHealth.tsx
+++ components/Controllers/Loans/VaultHealth.tsx
@@ -2,12 +2,16 @@ import React, { useMemo } from 'react';
 import { ethers } from 'ethers';
 import { convertOneScaledValue } from 'lib/controllers';
 import styles from './Loans.module.css';
+import { TooltipReference, useTooltipState } from 'reakit/Tooltip';
+import { Tooltip } from 'components/Tooltip';
+import { formatPercent } from 'lib/numberFormat';
 
 type VaultHealthProps = {
   ltv: number;
   maxLtv: ethers.BigNumber;
 };
 export function VaultHealth({ ltv, maxLtv }: VaultHealthProps) {
+  const healthTooltip = useTooltipState();
   const maxLTV = convertOneScaledValue(maxLtv, 4);
   const ratio = ltv / maxLTV;
 
@@ -20,38 +24,31 @@ export function VaultHealth({ ltv, maxLtv }: VaultHealthProps) {
   }, [ratio]);
 
   return (
-    <span
-      className={ratio > 0.5 ? styles['indicator-danger'] : styles.indicator}>
-      {indicator}
-    </span>
-  );
-}
-
-type NewVaultHealthProps = {
-  debt: ethers.BigNumber;
-  maxDebt: ethers.BigNumber;
-};
-
-// TODO(adamgobes): Migrate all old health components to use this new vault health component and rename
-export function NewVaultHealth({ debt, maxDebt }: NewVaultHealthProps) {
-  const ratio = Math.min(
-    1,
-    parseFloat(ethers.utils.formatEther(debt)) /
-      parseFloat(ethers.utils.formatEther(maxDebt)),
-  );
-
-  const indicator = useMemo(() => {
-    // Ratio, but as a number out of 10 rather than a decimal out of 1
-    const numHashes = Math.round(ratio * 10);
-    const dashes = Array(10).fill('-');
-    const hashes = Array(numHashes).fill('#');
-    return hashes.concat(dashes).join('').substring(0, 10);
-  }, [ratio]);
-
-  return (
-    <span
-      className={ratio > 0.5 ? styles['indicator-danger'] : styles.indicator}>
-      {indicator}
-    </span>
+    <>
+      <TooltipReference {...healthTooltip}>
+        <span
+          className={
+            ratio > 0.5 ? styles['indicator-danger'] : styles.indicator
+          }>
+          {indicator}
+        </span>
+      </TooltipReference>
+      <Tooltip {...healthTooltip}>
+        <div className={styles.tooltip}>
+          <div>
+            <span>Current LTV</span>
+            <span>{formatPercent(ltv)}</span>
+          </div>
+          <div>
+            <span>Max LTV</span>
+            <span>{formatPercent(maxLTV)}</span>
+          </div>
+          <div>
+            <span>Current/Max LTV</span>
+            <span>{formatPercent(ratio)}</span>
+          </div>
+        </div>
+      </Tooltip>
+    </>
   );
 }
