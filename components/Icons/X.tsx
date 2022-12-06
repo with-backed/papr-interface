4ab3b38553ad159d97f8835ef229b41c159463b0
Adam Gobran <adamgobes@gmail.com>
2022-12-06 13:53:57 -0500

    NFT-657: Show failed message with icon if tx fails (#218)
    * NFT-657: Show failed message with icon if tx fails
    
    * new icon
    
    * fix

---
+++ components/Icons/X.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+
+export const X = () => (
+  <svg
+    width="11"
+    height="11"
+    viewBox="0 0 11 11"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M1.82349 9.17643L9.17643 1.82349"
+      stroke="#DB004D"
+      strokeWidth="2"
+      strokeLinecap="round"
+    />
+    <path
+      d="M9.17643 9.17651L1.82349 1.82357"
+      stroke="#DB004D"
+      strokeWidth="2"
+      strokeLinecap="round"
+    />
+  </svg>
+);
