3043902195d7e345e79d38d0f04537270f0f8ec3
Christopher Nascone <chris@nasc.one>
2022-12-07 16:32:06 -0500

    fix: clean overview

--- components/Controllers/ControllerOverviewContent/ControllerOverviewContent.tsx
+++ components/Controllers/ControllerOverviewContent/ControllerOverviewContent.tsx
@@ -1,14 +1,13 @@
-import React, { useMemo } from 'react';
+import React, { ComponentProps, useMemo } from 'react';
 import styles from 'components/Controllers/Controller.module.css';
 import { ControllerPricesData } from 'lib/controllers/charts';
 import { PaprController } from 'lib/PaprController';
-import { Collateral } from 'components/Controllers/Collateral';
+import { Collateral as CollateralComponent } from 'components/Controllers/Collateral';
 import { Activity } from 'components/Controllers/Activity';
-import { Loans } from 'components/Controllers/Loans';
+import { Loans as LoansComponent } from 'components/Controllers/Loans';
 import { TokenPerformance } from 'components/Controllers/TokenPerformance';
 import dynamic from 'next/dynamic';
 import { Auctions } from 'components/Controllers/Auctions';
-import { YourPositions } from 'components/YourPositions/YourPositions';
 import { useCurrentVaults } from 'hooks/useCurrentVault/useCurrentVault';
 import { useAccount } from 'wagmi';
 import { useOracleInfo } from 'hooks/useOracleInfo/useOracleInfo';
@@ -16,11 +15,30 @@ import { OraclePriceType } from 'lib/oracle/reservoir';
 import { useAccountNFTs } from 'hooks/useAccountNFTs';
 import { useConfig } from 'hooks/useConfig';
 import { usePaprBalance } from 'hooks/usePaprBalance';
+import { YourPositions as YourPositionsComponent } from 'components/YourPositions';
 
 /* lightweight-charts uses canvas and cannot be SSRed */
 const Charts = dynamic(() => import('components/Controllers/Charts/Charts'), {
   ssr: false,
 });
+const YourPositions = dynamic<ComponentProps<typeof YourPositionsComponent>>(
+  () => import('components/YourPositions').then((mod) => mod.YourPositions),
+  {
+    ssr: false,
+  },
+);
+const Loans = dynamic<ComponentProps<typeof LoansComponent>>(
+  import('components/Controllers/Loans').then((mod) => mod.Loans),
+  {
+    ssr: false,
+  },
+);
+const Collateral = dynamic<ComponentProps<typeof CollateralComponent>>(
+  import('components/Controllers/Collateral').then((mod) => mod.Collateral),
+  {
+    ssr: false,
+  },
+);
 
 export type ControllerPageProps = {
   paprController: PaprController;
