20a18f913ec6d2aa36796ea29380ec6a8d4c95d2
Christopher Nascone <chris@nasc.one>
2022-12-16 16:20:04 -0500

    fix: remove dead code

--- lib/pAPRSubgraph.ts
+++ lib/pAPRSubgraph.ts
@@ -5,12 +5,9 @@ import {
   PaprControllerByIdQuery,
   VaultsByOwnerForControllerDocument,
   VaultsByOwnerForControllerQuery,
-  PaprHeroPlayersDocument,
-  PaprHeroPlayersQuery,
-  User,
 } from 'types/generated/graphql/inKindSubgraph';
 import { configs, SupportedToken } from './config';
-import { PaprController, SubgraphController } from './PaprController';
+import { SubgraphController } from './PaprController';
 import { clientFromUrl } from './urql';
 
 export async function subgraphControllerByAddress(
@@ -45,22 +42,6 @@ export async function getAllPaprControllers(
   }
 
   return data?.paprControllers || [];
-}
-
-export async function getAllPaprHeroPlayers(
-  token: SupportedToken,
-): Promise<User[] | null> {
-  const client = clientFromUrl(configs[token].paprMemeSubgraph);
-  const { data, error } = await client
-    .query<PaprHeroPlayersQuery>(PaprHeroPlayersDocument, {})
-    .toPromise();
-
-  if (error) {
-    console.error(error);
-    return [];
-  }
-
-  return data?.users || [];
 }
 
 export async function getAllVaultsForControllerForUser(
