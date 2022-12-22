34486f32d83ffddc7da56fb435f33edf3e890f4f
Christopher Nascone <chris@nasc.one>
2022-12-22 10:52:19 -0500

    fix: types

--- components/Controllers/Loans/LoanDetails.tsx
+++ components/Controllers/Loans/LoanDetails.tsx
@@ -2,11 +2,8 @@ import React, { useMemo } from 'react';
 import { Fieldset } from 'components/Fieldset';
 import { VaultRow } from 'components/Controllers/Loans/VaultRow';
 import { PaprController } from 'lib/PaprController';
-import { useAsyncValue } from 'hooks/useAsyncValue';
 import styles from './Loans.module.css';
-import { computeLtv, convertOneScaledValue } from 'lib/controllers';
 import { Table } from 'components/Table';
-import { ethers } from 'ethers';
 
 type LoanDetailsProps = {
   vaultId: string;
@@ -17,21 +14,7 @@ export function LoanDetails({ paprController, vaultId }: LoanDetailsProps) {
   const vault = useMemo(() => {
     return paprController.vaults?.find((v) => v.id === vaultId);
   }, [paprController, vaultId]);
-  const norm = useAsyncValue(
-    () => paprController.newTarget(),
-    [paprController],
-  );
   const maxLTV = useMemo(() => paprController.maxLTVBigNum, [paprController]);
-  const ltv = useMemo(() => {
-    if (vault && norm) {
-      return convertOneScaledValue(
-        // TODO, punting on borrow page rework
-        computeLtv(vault.debt, 1, norm),
-        4,
-      );
-    }
-    return undefined;
-  }, [vault, norm]);
   return (
     <Fieldset legend={'💸 Loan Details'}>
       <Table className={styles.table}>
@@ -47,10 +30,8 @@ export function LoanDetails({ paprController, vaultId }: LoanDetailsProps) {
           <VaultRow
             id={vaultId}
             account={vault?.account}
-            debt={vault!.debt}
             maxLTV={maxLTV}
-            ltv={ltv}
-            controllerId={paprController.id}
+            paprController={paprController}
           />
         </tbody>
       </Table>
