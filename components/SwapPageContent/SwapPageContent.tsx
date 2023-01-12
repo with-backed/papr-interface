7610787f6b7fbe94d0ad643edc481a7ccc47f0aa
Christopher Nascone <chris@nasc.one>
2023-01-12 14:08:52 -0500

    wip: latest

--- components/SwapPageContent/SwapPageContent.tsx
+++ components/SwapPageContent/SwapPageContent.tsx
@@ -1,11 +1,38 @@
-import { SwapWidget } from '@uniswap/widgets';
+import { SwapWidget, Theme } from '@uniswap/widgets';
 import '@uniswap/widgets/fonts.css';
+import { useConfig } from 'hooks/useConfig';
+import { useMemo } from 'react';
+import { useProvider } from 'wagmi';
 import styles from './SwapPageContent.module.css';
 
+const theme: Theme = {
+  primary: '#000',
+  secondary: '#000',
+  interactive: '#C6E6E1',
+  container: '#FFF',
+  module: '#F2F9F8',
+  accent: '#0000EE',
+  outline: '#000',
+  dialog: '#000',
+  fontFamily: 'Courier Prime',
+  borderRadius: 0.5,
+};
+
 export function SwapPageContent() {
+  const { chainId, jsonRpcProvider } = useConfig();
+  const provider = useProvider();
+
+  const jsonRpcUrlMap = useMemo(
+    () => ({ [chainId]: jsonRpcProvider }),
+    [chainId, jsonRpcProvider],
+  );
   return (
     <div className={styles.wrapper}>
-      <SwapWidget />
+      <SwapWidget
+        theme={theme}
+        jsonRpcUrlMap={jsonRpcUrlMap}
+        provider={provider as any}
+      />
     </div>
   );
 }
