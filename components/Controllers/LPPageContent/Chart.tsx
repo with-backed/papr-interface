efef0141aa4d6da69080e8ebb9093e3d9d5460ce
Christopher Nascone <chris@nasc.one>
2023-01-19 16:58:58 -0500

    wip: chart styling

--- components/Controllers/LPPageContent/Chart.tsx
+++ components/Controllers/LPPageContent/Chart.tsx
@@ -1,17 +1,21 @@
+import { TextButton } from 'components/Button';
 import { usePoolChartData } from 'hooks/usePoolChartData';
+import { useTheme } from 'hooks/useTheme';
 import { ChartOptions, createChart, DeepPartial } from 'lightweight-charts';
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import styles from './LPPageContent.module.css';
 
 type ChartView = 'volumeUSD' | 'liquidityUSD' | 'feesUSD';
 
 const BASE_CHART_OPTIONS: DeepPartial<ChartOptions> = {
-  height: 360,
-  width: 500,
   grid: { horzLines: { visible: false }, vertLines: { visible: false } },
   layout: {
     textColor: 'black',
     fontFamily: "'Courier Prime', 'Courier New', monospace",
     fontSize: 12,
+  },
+  rightPriceScale: {
+    visible: false,
   },
 };
 
@@ -19,24 +23,84 @@ export function Chart() {
   const [view, setView] = useState<ChartView>('volumeUSD');
   const chartRef = useRef<HTMLDivElement>(null);
   const { chartData, allFound } = usePoolChartData();
+  const theme = useTheme();
+
+  const primaryColor = useMemo(() => {
+    return {
+      meme: '#47b79c',
+      trash: '#ff6333',
+      hero: '#908ee7',
+      papr: '#5397ff',
+    }[theme];
+  }, [theme]);
 
   const series = useMemo(
-    () => chartData.map(({ time, ...props }) => ({ time, value: props[view] })),
-    [chartData, view],
+    () =>
+      chartData.map(({ time, ...props }) => ({
+        time,
+        value: /*props[view]*/ (Math.random() * 1000) | 0,
+      })),
+    [chartData],
   );
 
   useEffect(() => {
     if (chartRef.current) {
       const chart = createChart(chartRef.current, { ...BASE_CHART_OPTIONS });
-      const histogramSeries = chart.addHistogramSeries();
+      const histogramSeries = chart.addHistogramSeries({
+        color: primaryColor,
+        priceLineVisible: false,
+      });
       histogramSeries.setData(series as any);
       chart.timeScale().fitContent();
 
       return () => chart.remove();
     }
-  }, [series]);
+  }, [primaryColor, series]);
 
-  console.log({ series });
+  const viewSelectorFactory = useCallback(
+    (view: ChartView) => () => setView(view),
+    [],
+  );
+  const [handleVolumeClick, handleLiquidityClick, handleFeesClick] = useMemo(
+    () => [
+      viewSelectorFactory('volumeUSD'),
+      viewSelectorFactory('liquidityUSD'),
+      viewSelectorFactory('feesUSD'),
+    ],
+    [viewSelectorFactory],
+  );
 
-  return <div ref={chartRef}></div>;
+  return (
+    <div className={styles['chart-wrapper']}>
+      <div className={styles['chart-controls']}>
+        CHART:
+        <TextButton
+          onClick={handleVolumeClick}
+          className={
+            styles[
+              view === 'volumeUSD' ? 'chart-button-active' : 'chart-button'
+            ]
+          }>
+          Volume
+        </TextButton>
+        <TextButton
+          onClick={handleLiquidityClick}
+          className={
+            styles[
+              view === 'liquidityUSD' ? 'chart-button-active' : 'chart-button'
+            ]
+          }>
+          Liquidity
+        </TextButton>
+        <TextButton
+          onClick={handleFeesClick}
+          className={
+            styles[view === 'feesUSD' ? 'chart-button-active' : 'chart-button']
+          }>
+          Fees
+        </TextButton>
+      </div>
+      <div ref={chartRef} className={styles.chart}></div>
+    </div>
+  );
 }
