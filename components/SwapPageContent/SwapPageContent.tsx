41fe5c5c66eddf9522bff1c6866fbd16ebffed4c
Christopher Nascone <chris@nasc.one>
2023-01-27 10:29:04 -0500

    feat: customize tokens

--- components/SwapPageContent/SwapPageContent.tsx
+++ components/SwapPageContent/SwapPageContent.tsx
@@ -2,6 +2,7 @@ import { JsonRpcSigner } from '@ethersproject/providers';
 import { SwapWidget, Theme } from '@uniswap/widgets';
 import '@uniswap/widgets/fonts.css';
 import { useConfig } from 'hooks/useConfig';
+import { useController } from 'hooks/useController';
 import { useMemo } from 'react';
 import { useSigner } from 'wagmi';
 import styles from './SwapPageContent.module.css';
@@ -20,19 +21,44 @@ const theme: Theme = {
 };
 
 export function SwapPageContent() {
-  const { chainId, jsonRpcProvider } = useConfig();
+  const { paprToken, underlying } = useController();
+  const { chainId, jsonRpcProvider, tokenName } = useConfig();
   const provider = useSigner<JsonRpcSigner>().data?.provider;
 
   const jsonRpcUrlMap = useMemo(
     () => ({ [chainId]: jsonRpcProvider }),
     [chainId, jsonRpcProvider],
   );
+
+  const tokenList = useMemo(
+    () => [
+      {
+        address: underlying.id,
+        chainId,
+        decimals: underlying.decimals,
+        name: 'USD Coin',
+        symbol: 'USDC',
+      },
+      {
+        address: paprToken.id,
+        chainId,
+        decimals: paprToken.decimals,
+        name: tokenName,
+        symbol: tokenName,
+      },
+    ],
+    [chainId, paprToken, tokenName, underlying],
+  );
+
   return (
     <div className={styles.wrapper}>
       <SwapWidget
         theme={theme}
         jsonRpcUrlMap={jsonRpcUrlMap}
         provider={provider}
+        tokenList={tokenList}
+        defaultInputTokenAddress={paprToken.id}
+        defaultOutputTokenAddress={underlying.id}
       />
     </div>
   );
--- pages/tokens/[token]/swap/index.tsx
+++ pages/tokens/[token]/swap/index.tsx
@@ -1,39 +1,56 @@
 import { GetServerSideProps } from 'next';
 import React from 'react';
 import { captureException } from '@sentry/nextjs';
-import { SupportedToken, validateToken } from 'lib/config';
+import { configs, getConfig, SupportedToken } from 'lib/config';
 import { OpenGraph } from 'components/OpenGraph';
 import capitalize from 'lodash/capitalize';
 import { SwapPageContent } from 'components/SwapPageContent';
+import { fetchSubgraphData, SubgraphController } from 'lib/PaprController';
+import { ControllerContextProvider } from 'hooks/useController';
 
 export const getServerSideProps: GetServerSideProps<SwapProps> = async (
   context,
 ) => {
-  try {
-    validateToken(context.params || {});
-    const token = context.params?.token as SupportedToken;
-
-    return {
-      props: {
-        token,
-      },
-    };
-  } catch (e) {
-    captureException(e);
+  const token = context.params?.token as SupportedToken;
+  const address: string | undefined =
+    getConfig(token)?.controllerAddress?.toLocaleLowerCase();
+  if (!address) {
     return {
       notFound: true,
     };
   }
+
+  const controllerSubgraphData = await fetchSubgraphData(
+    address,
+    configs[token].uniswapSubgraph,
+    token,
+  );
+
+  if (!controllerSubgraphData) {
+    const e = new Error(`subgraph data for controller ${address} not found`);
+    captureException(e);
+    throw e;
+  }
+
+  const { paprController } = controllerSubgraphData;
+
+  return {
+    props: {
+      subgraphController: paprController,
+      token,
+    },
+  };
 };
 
 type SwapProps = {
   token: SupportedToken;
+  subgraphController: SubgraphController;
 };
-export default function Swap({ token }: SwapProps) {
+export default function Swap({ subgraphController, token }: SwapProps) {
   return (
-    <>
+    <ControllerContextProvider value={subgraphController}>
       <OpenGraph title={`Backed | ${capitalize(token)} | Swap`} />
       <SwapPageContent />
-    </>
+    </ControllerContextProvider>
   );
 }
