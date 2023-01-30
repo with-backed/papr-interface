7411c6eb71d1a12665f065744cc9672c43a1e2f7
wilsoncusack <cusackw@gmail.com>
2023-01-30 15:49:02 -0500

    update paprHero config (#285)

--- lib/config.ts
+++ lib/config.ts
@@ -52,13 +52,13 @@ const paprHero: Config = {
   etherscanUrl: 'https://goerli.etherscan.io',
   siteUrl: 'https://staging.withbacked.xyz',
   network: 'goerli',
-  controllerAddress: '0x1c262eca411891f984719edb9931354846e61756',
-  underlyingAddress: '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6',
-  paprTokenAddress: '0xbd3c7f5a433e9643eb0dd73880c902bc21734b15',
+  controllerAddress: '0xd50004ec23ad97cf3a3bbd9718ad9bd274dc1764',
+  underlyingAddress: '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6 ',
+  paprTokenAddress: '0x9E1fb473788BbA070F3cd19fF1E9bBBFA776a477',
   uniswapSubgraph:
     'https://api.thegraph.com/subgraphs/name/liqwiz/uniswap-v3-goerli',
   paprMemeSubgraph: goerliSubgraph,
-  paprUnderlyingAddress: '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6',
+  paprUnderlyingAddress: '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
   reservoirAPI: 'https://api-goerli.reservoir.tools',
   reservoirMarketplace: 'https://goerli-marketplace-gules.vercel.app',
   erc721Subgraph:
