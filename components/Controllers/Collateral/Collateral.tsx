eab0d6a4c67425bc90175786bacb53a472cc6ba9
Adam Gobran <adamgobes@gmail.com>
2023-02-06 15:44:06 -0500

    NFT-843: Collateral stats

--- components/Controllers/Collateral/Collateral.tsx
+++ components/Controllers/Collateral/Collateral.tsx
@@ -6,13 +6,16 @@ import { Tooltip } from 'components/Tooltip';
 import { ethers } from 'ethers';
 import { useConfig } from 'hooks/useConfig';
 import { PaprController, useController } from 'hooks/useController';
+import { useLatestMarketPrice } from 'hooks/useLatestMarketPrice';
 import { useMaxDebt } from 'hooks/useMaxDebt';
+import { useOracleInfo } from 'hooks/useOracleInfo/useOracleInfo';
 import { useShowMore } from 'hooks/useShowMore';
 import { computeLTVFromDebts } from 'lib/controllers';
 import { formatPercent } from 'lib/numberFormat';
 import { OraclePriceType } from 'lib/oracle/reservoir';
 import React, { useMemo } from 'react';
 import { TooltipReference, useTooltipState } from 'reakit';
+import { erc20ABI, useContractRead } from 'wagmi';
 
 import styles from './Collateral.module.css';
 
@@ -26,6 +29,8 @@ const COLLATERAL_INCREMENT = 30;
 
 export function Collateral({ vaultId }: CollateralProps) {
   const paprController = useController();
+  const oracleInfo = useOracleInfo(OraclePriceType.twap);
+  const latestMarketPrice = useLatestMarketPrice();
   const vaults = useMemo(() => {
     if (vaultId) {
       const vault = paprController.vaults?.find((v) => v.id === vaultId);
@@ -42,6 +47,36 @@ export function Collateral({ vaultId }: CollateralProps) {
     [vaults],
   );
 
+  const totalCollateralValue = useMemo(() => {
+    if (!oracleInfo) return null;
+    return collateral
+      .map(({ vault }) => oracleInfo[vault.token.id].price)
+      .reduce((a, b) => a + b, 0);
+  }, [collateral, oracleInfo]);
+  const { data: totalSupply } = useContractRead({
+    abi: erc20ABI,
+    address: paprController.paprToken.id as `0x${string}`,
+    functionName: 'totalSupply',
+  });
+  const totalSupplyInUnderlying = useMemo(() => {
+    if (!totalSupply || !latestMarketPrice) return null;
+    return (
+      parseFloat(
+        ethers.utils.formatUnits(
+          totalSupply,
+          paprController.paprToken.decimals,
+        ),
+      ) * latestMarketPrice
+    );
+  }, [latestMarketPrice, paprController.paprToken.decimals, totalSupply]);
+  const collateralizationRatio = useMemo(() => {
+    if (!totalCollateralValue || !totalSupply) return null;
+    const totalSupplyNum = parseFloat(
+      ethers.utils.formatUnits(totalSupply, paprController.paprToken.decimals),
+    );
+    return totalCollateralValue / totalSupplyNum;
+  }, [totalCollateralValue, totalSupply, paprController.paprToken.decimals]);
+
   const { feed, remainingLength, showMore, amountThatWillShowNext } =
     useShowMore(collateral, COLLATERAL_INCREMENT);
 
@@ -55,6 +90,47 @@ export function Collateral({ vaultId }: CollateralProps) {
 
   return (
     <Fieldset legend="🖼 Collateral">
+      <div className={styles.stats}>
+        <div>
+          <div>
+            <p>Floor value of all deposited collateral</p>
+          </div>
+          <div>
+            {!totalCollateralValue && <p>...</p>}
+            {totalCollateralValue && (
+              <p>
+                {totalCollateralValue.toFixed(4)}{' '}
+                {paprController.underlying.symbol}
+              </p>
+            )}
+          </div>
+        </div>
+        <div>
+          <div>
+            <p>Market value of all {paprController.paprToken.symbol} tokens</p>
+          </div>
+          <div>
+            {!totalSupplyInUnderlying && <p>...</p>}
+            {totalSupplyInUnderlying && (
+              <p>
+                {totalSupplyInUnderlying.toFixed(4)}{' '}
+                {paprController.underlying.symbol}
+              </p>
+            )}
+          </div>
+        </div>
+        <div>
+          <div>
+            <p>Collateralization ratio (NFT value/token value)</p>
+          </div>
+          <div>
+            {!collateralizationRatio && <p>...</p>}
+            {collateralizationRatio && (
+              <p>{collateralizationRatio.toFixed(2)}</p>
+            )}
+          </div>
+        </div>
+      </div>
       <div className={styles.wrapper}>
         {feed.map(({ vault: v, collateral: c }) => (
           <Tile key={c.id} address={v.token.id} tokenId={c.tokenId} vault={v} />
