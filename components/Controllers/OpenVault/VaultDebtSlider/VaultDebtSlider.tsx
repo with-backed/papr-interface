13110ac5539d300c227082cb487ef5db49d78f02
Adam Gobran <adamgobes@gmail.com>
2023-02-08 13:59:16 -0500

    chore: refactor

--- components/Controllers/OpenVault/VaultDebtSlider/VaultDebtSlider.tsx
+++ components/Controllers/OpenVault/VaultDebtSlider/VaultDebtSlider.tsx
@@ -35,9 +35,8 @@ export function VaultDebtSlider({
 
   const initBlackTrackWidth = useCallback(
     (val: string) => {
-      if (!blackTrackWidth[0] && val !== '0px') {
+      if (!blackTrackWidth[0] && val !== '0px')
         setBlackTrackWidth([val, maxDebtNumber]);
-      }
     },
     [blackTrackWidth, maxDebtNumber],
   );
