67d27d847b8876c93a949f17dfbf696586a87f47
Christopher Nascone <chris@nasc.one>
2023-02-10 14:01:03 -0500

    fix: filter duplicate timestamps on chart

--- hooks/useControllerPricesData/useControllerPricesData.ts
+++ hooks/useControllerPricesData/useControllerPricesData.ts
@@ -170,6 +170,7 @@ function targets(
       time: t as UTCTimestamp,
     };
   });
+  console.log({ formattedTargets });
   return formattedTargets;
 }
 
@@ -207,5 +208,15 @@ function marks(
     });
   }
 
-  return formattedSwaps.filter(({ time }) => time !== OUTLIER_TIMESTAMP);
+  const result: typeof formattedSwaps = [];
+  const seenTimestamps = new Set([OUTLIER_TIMESTAMP]);
+  formattedSwaps.forEach((swap) => {
+    if (seenTimestamps.has(swap.time)) {
+      return;
+    }
+    seenTimestamps.add(swap.time);
+    result.push(swap);
+  });
+
+  return result;
 }
