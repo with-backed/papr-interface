81d3a8f702ed70864e91594f5d153bd965f69e8e
Adam Gobran <adamgobes@gmail.com>
2023-02-13 15:27:15 -0500

    NFT-942: Fix incorrect message on VaultWriteButton

--- components/Controllers/OpenVault/VaultWriteButton.tsx
+++ components/Controllers/OpenVault/VaultWriteButton.tsx
@@ -18,6 +18,7 @@ type VaultWriteButtonProps = {
   collateralContractAddress: string;
   depositNFTs: string[];
   withdrawNFTs: string[];
+  vaultCollateralCount: number;
   amount: ethers.BigNumber;
   quote: ethers.BigNumber | null;
   vaultHasDebt: boolean;
@@ -31,6 +32,7 @@ export function VaultWriteButton({
   collateralContractAddress,
   depositNFTs,
   withdrawNFTs,
+  vaultCollateralCount,
   amount,
   quote,
   vaultHasDebt,
@@ -143,8 +145,9 @@ export function VaultWriteButton({
 
   const buttonText = useMemo(() => {
     if (!oracleInfoSynced) return 'Waiting for oracle...';
-    return vaultHasDebt ? 'Update Loan' : 'Borrow';
-  }, [oracleInfoSynced, vaultHasDebt]);
+    else if (vaultHasDebt || vaultCollateralCount > 0) return 'Update Loan';
+    return 'Borrow';
+  }, [oracleInfoSynced, vaultHasDebt, vaultCollateralCount]);
 
   return (
     <>
