25ddabdd6671ddac384149c2b0c0308615d20e15
Christopher Nascone <chris@nasc.one>
2023-02-14 08:39:31 -0500

    chore: remove unused deps

--- types/pinata.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '@pinata/ipfs-gateway-tools/dist/node' {
-  export default class IPFSGatewayTools {
-    containsCID(url: string): { containsCid: boolean; cid?: string };
-    convertToDesiredGateway(
-      sourceUrl: string,
-      desiredGatewayPrefix: string,
-    ): string;
-  }
-}
