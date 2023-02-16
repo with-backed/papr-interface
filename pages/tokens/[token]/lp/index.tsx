cc02c3c210d9380b71d706e1d25da5f299e286cc
Christopher Nascone <chris@nasc.one>
2023-02-16 17:00:55 -0500

    debug: no server-side at all

--- pages/tokens/[token]/lp/index.tsx
+++ pages/tokens/[token]/lp/index.tsx
@@ -4,25 +4,8 @@ import { PageLevelStatusFieldsets } from 'components/StatusFieldset/PageLevelSta
 import { useConfig } from 'hooks/useConfig';
 import { ControllerContextProvider } from 'hooks/useController';
 import { useSubgraphData } from 'hooks/useSubgraphData';
-import { getConfig, SupportedToken } from 'lib/config';
 import capitalize from 'lodash/capitalize';
-import { GetServerSideProps } from 'next';
 import React from 'react';
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const token = context.params?.token as SupportedToken;
-  const address: string | undefined =
-    getConfig(token)?.controllerAddress?.toLocaleLowerCase();
-  if (!address) {
-    return {
-      notFound: true,
-    };
-  }
-
-  return {
-    props: {},
-  };
-};
 
 export default function LP() {
   const { network } = useConfig();
