8999ce9cab2fe1ae574e514442fc7fce85d90130
Adam Gobran <adamgobes@gmail.com>
2023-02-17 12:38:01 -0500

    more logging

--- pages/api/events/ingest.ts
+++ pages/api/events/ingest.ts
@@ -1,7 +1,11 @@
+import { WebhookRequestBody } from 'lib/goldsky/webhooks';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log({ body: req.body });
+  const requestBody = req.body as WebhookRequestBody;
+  console.log({
+    data: requestBody.data.new,
+  });
   return res.status(200).json({ ok: true });
 }
 
