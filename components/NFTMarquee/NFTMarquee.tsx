036fce02cecbf5f34bd8248eaf830b9d70123d91
Christopher Nascone <chris@nasc.one>
2023-03-01 09:14:15 -0500

    fix: all images scroll at same rate

--- components/NFTMarquee/NFTMarquee.tsx
+++ components/NFTMarquee/NFTMarquee.tsx
@@ -2,7 +2,6 @@ import { useCollection } from '@center-inc/react';
 import { CenterAsset } from 'components/CenterAsset';
 import { Tooltip } from 'components/Tooltip';
 import { useConfig } from 'hooks/useConfig';
-import { useMemo } from 'react';
 import Marquee from 'react-fast-marquee';
 import { TooltipReference, useTooltipState } from 'reakit/Tooltip';
 import { VaultCollateral } from 'types/generated/graphql/inKindSubgraph';
@@ -13,10 +12,6 @@ type NFTMarqueeProps = {
   collateral: VaultCollateral[];
 };
 
-// images are 50px wide
-const IMAGE_WIDTH = 50;
-const SCROLL_DURATION = 7; // seconds
-
 export function NFTMarquee({ collateral }: NFTMarqueeProps) {
   const { centerNetwork } = useConfig();
   const result = useCollection({
@@ -24,18 +19,15 @@ export function NFTMarquee({ collateral }: NFTMarqueeProps) {
     address: collateral[0].id.split('-')[0],
   });
   const tooltip = useTooltipState({ placement: 'right' });
-  const pxPerSecond = useMemo(() => {
-    return Math.floor((collateral.length * IMAGE_WIDTH) / SCROLL_DURATION);
-  }, [collateral]);
 
   return (
     <div className={styles.wrapper}>
       <TooltipReference {...tooltip}>
         <Marquee
           style={{ width: '50px', height: '50px', overflow: 'hidden' }}
-          speed={pxPerSecond}
           play={collateral.length > 1}
           pauseOnHover
+          speed={10}
           // gradient messed with our CSS
           gradient={false}>
           {collateral.map((c) => (
