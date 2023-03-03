7d9ee59ea6ecb985520ca63931c49d6552a711f4
Christopher Nascone <chris@nasc.one>
2023-03-03 10:07:34 -0500

    feat: new HealthBar component

---
+++ components/HealthBar/HealthBar.stories.tsx
@@ -0,0 +1,15 @@
+import { HealthBar } from './HealthBar';
+
+export const HealthBars = () => {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
+      <HealthBar ratio={0} />
+      <HealthBar ratio={0.25} />
+      <HealthBar ratio={0.5} />
+      <HealthBar ratio={0.75} />
+      <HealthBar ratio={0.95} />
+      <HealthBar ratio={1} />
+      <HealthBar ratio={25} />
+    </div>
+  );
+};
