4ff63e3f51cd89fbc88626d2b48e1dce181c6ccb
Christopher Nascone <cnasc@users.noreply.github.com>
2023-03-03 10:40:18 -0500

    feat: new HealthBar component (#410)
    * feat: new HealthBar component
    
    * chore: use HealthBar in VaultHealth
    
    * fix: handle OOB input
    
    * fix: graph

---
+++ components/HealthBar/HealthBar.tsx
@@ -0,0 +1,40 @@
+import { useMemo } from 'react';
+
+import styles from './HealthBar.module.css';
+
+const DEFAULT_NUM_SEGMENTS = 16;
+const DEFAULT_UNHEALTHY_THRESHOLD = 0.85;
+
+type HealthBarProps = {
+  numSegments?: number;
+  ratio: number;
+  threshold?: number;
+};
+
+export function HealthBar({
+  numSegments = DEFAULT_NUM_SEGMENTS,
+  ratio,
+  threshold = DEFAULT_UNHEALTHY_THRESHOLD,
+}: HealthBarProps) {
+  const segments = useMemo(() => {
+    // Ratio must be a number between zero and 1 (inclusive)
+    const clampedRatio = Math.min(1, Math.max(0, ratio));
+
+    // Ratio, but as a number out of numSegments rather than a decimal out of 1
+    const numFilledSegments = Math.round(clampedRatio * numSegments);
+
+    const empty = Array<string>(numSegments).fill('░');
+    const filled = Array<string>(numFilledSegments).fill('▓');
+
+    return filled.concat(empty).slice(0, numSegments).join('');
+  }, [numSegments, ratio]);
+
+  return (
+    <span
+      className={
+        ratio > threshold ? styles['indicator-danger'] : styles.indicator
+      }>
+      {segments}
+    </span>
+  );
+}
