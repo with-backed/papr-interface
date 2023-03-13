561c2624da7df925ed4de0d4402941139dd40625
Christopher Nascone <chris@nasc.one>
2023-03-13 16:58:43 -0400

    feat: correct display of debt in tooltip

--- components/Controllers/Loans/VaultHealth.tsx
+++ components/Controllers/Loans/VaultHealth.tsx
@@ -3,8 +3,14 @@ import { Tooltip } from 'components/Tooltip';
 import { ethers } from 'ethers';
 import { useController } from 'hooks/useController';
 import { useLTV } from 'hooks/useLTV';
+import { useTarget } from 'hooks/useTarget';
 import { convertOneScaledValue } from 'lib/controllers';
-import { formatPercent } from 'lib/numberFormat';
+import {
+  formatPercent,
+  formatPercentChange,
+  formatTokenAmount,
+} from 'lib/numberFormat';
+import { percentChange } from 'lib/tokenPerformance';
 import React, { useMemo } from 'react';
 import { TooltipReference, useTooltipState } from 'reakit/Tooltip';
 import { SubgraphVault } from 'types/SubgraphVault';
@@ -16,15 +22,21 @@ type VaultHealthProps = {
 };
 export function VaultHealth({ vault }: VaultHealthProps) {
   const healthTooltip = useTooltipState();
-  const { maxLTV } = useController();
+  const { maxLTV, paprToken } = useController();
   const ltv = useLTV(vault?.token.id, vault?.collateralCount, vault.debt);
-  const formattedMaxLTV = convertOneScaledValue(
-    ethers.BigNumber.from(maxLTV),
-    4,
+  const formattedMaxLTV = useMemo(
+    () => convertOneScaledValue(ethers.BigNumber.from(maxLTV), 4),
+    [maxLTV],
   );
+
   const ratio = useMemo(
     () => (ltv ? ltv / formattedMaxLTV : 0),
     [formattedMaxLTV, ltv],
+  );
+
+  const debtNumber = useMemo(
+    () => parseFloat(ethers.utils.formatUnits(vault.debt, paprToken.decimals)),
+    [paprToken, vault.debt],
   );
 
   return (
@@ -33,7 +45,11 @@ export function VaultHealth({ vault }: VaultHealthProps) {
         <HealthBar ratio={ratio} />
       </TooltipReference>
       <Tooltip {...healthTooltip}>
-        <VaultHealthTooltipContent ltv={ltv} maxLtv={formattedMaxLTV} />
+        <VaultHealthTooltipContent
+          ltv={ltv}
+          maxLtv={formattedMaxLTV}
+          debt={debtNumber}
+        />
       </Tooltip>
     </>
   );
@@ -42,16 +58,45 @@ export function VaultHealth({ vault }: VaultHealthProps) {
 type VaultHealthTooltipContentProps = {
   ltv: number | null;
   maxLtv: number;
-  debt: string;
-  debtPercentChange: string;
+  debt: number;
 };
 
 function VaultHealthTooltipContent({
   ltv,
   maxLtv,
   debt,
-  debtPercentChange,
 }: VaultHealthTooltipContentProps) {
+  const { paprToken } = useController();
+  const targetNow = useTarget();
+  const debtNumberNow = useMemo(() => {
+    if (targetNow) {
+      const targetNum = parseFloat(
+        ethers.utils.formatUnits(targetNow.target, paprToken.decimals),
+      );
+      return debt * targetNum;
+    }
+    return null;
+  }, [debt, paprToken, targetNow]);
+
+  const targetYesterday = useTarget('yesterday');
+  const debtPercentChange = useMemo(() => {
+    if (targetYesterday && debtNumberNow) {
+      const targetNum = parseFloat(
+        ethers.utils.formatUnits(targetYesterday.target, paprToken.decimals),
+      );
+      const debtNumberYesterday = debt * targetNum;
+      return formatPercentChange(
+        percentChange(debtNumberYesterday, debtNumberNow),
+      );
+    }
+    return null;
+  }, [debt, debtNumberNow, paprToken, targetYesterday]);
+
+  const formattedDebtNow = useMemo(
+    () => (debtNumberNow ? formatTokenAmount(debtNumberNow) + ' ETH' : '...'),
+    [debtNumberNow],
+  );
+
   return (
     <div className={styles.tooltip}>
       <span>LTV calculation:</span>
@@ -59,7 +104,7 @@ function VaultHealthTooltipContent({
       <span className={styles.right}>Δ 24hr</span>
 
       <span>Debt (papr borrowed x Target)</span>
-      <span>{debt}</span>
+      <span>{formattedDebtNow}</span>
       <span>({debtPercentChange})</span>
 
       <span>Collateral (7-day avg. top bid)</span>
