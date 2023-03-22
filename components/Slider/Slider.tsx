e1bdfee020085759e1063c471746fa7bf550f156
Christopher Nascone <chris@nasc.one>
2023-03-22 10:47:37 -0400

    feat: slider indicates risk

--- components/Slider/Slider.tsx
+++ components/Slider/Slider.tsx
@@ -5,6 +5,13 @@ import ReactSlider from 'react-slider';
 
 import styles from './Slider.module.css';
 
+/**
+ * When creating a loan we want to highlight the risk of pushing LTV
+ * too high. We have decided that 45% is the point to highlight, which
+ * is 90% of the slider's range.
+ */
+const DANGER_THRESHOLD = 0.9;
+
 export type SliderProps = {
   min: number;
   max: number;
@@ -52,6 +59,26 @@ export function Slider({
     [onAfterChange],
   );
 
+  const isRisky = useMemo(() => {
+    return value ? value / max >= DANGER_THRESHOLD : false;
+  }, [max, value]);
+
+  const trackClassName = useMemo(() => {
+    const base = [styles.track, styles[theme]];
+    if (isRisky) {
+      return [...base, styles.danger].join(' ');
+    }
+    return base.join(' ');
+  }, [isRisky, theme]);
+
+  const thumbClassName = useMemo(() => {
+    const base = [styles.thumb, styles[theme]];
+    if (isRisky) {
+      return [...base, styles.danger].join(' ');
+    }
+    return base.join(' ');
+  }, [isRisky, theme]);
+
   return (
     <>
       <div
@@ -66,8 +93,8 @@ export function Slider({
         }}></div>
       <ReactSlider
         className={[styles.slider, styles[theme]].join(' ')}
-        trackClassName={[styles.track, styles[theme]].join(' ')}
-        thumbClassName={[styles.thumb, styles[theme]].join(' ')}
+        trackClassName={trackClassName}
+        thumbClassName={thumbClassName}
         renderThumb={renderThumb}
         renderTrack={renderTrack}
         min={min}
