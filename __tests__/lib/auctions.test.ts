171bb8ff31cf1e3ad43b3b6ab63a41d66b697261
Adam Gobran <adamgobes@gmail.com>
2023-04-10 14:00:56 -0400

    update another test

--- __tests__/lib/auctions.test.ts
+++ __tests__/lib/auctions.test.ts
@@ -7,7 +7,10 @@ const oneDayInSeconds = 60 * 60 * 24;
 const startPrice = ethers.BigNumber.from(10).pow(18);
 const perPeriodDecayPercentWad = convertOneScaledValue(ONE.mul(9).div(10), 8);
 
-function approximatelyEquals(n1: ethers.BigNumber, n2: ethers.BigNumber) {
+export function approximatelyEquals(
+  n1: ethers.BigNumber,
+  n2: ethers.BigNumber,
+) {
   const tolerance = ethers.utils.parseEther('0.00000001');
   return n1.sub(n2).abs().lt(tolerance);
 }
